af8ff386be9c13c48dfaba97345c285d5a28f7d6
Ankit Tewari <ankt271192@users.noreply.github.com>
2018-06-02 00:32:53 +0530

    Add onSubmit event handler for StorePicker form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required />
+        <input
+          ref={this.myInput}
+          type="text"
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          required
+        />
         <button type="submit">Visit Store →</button>
       </form>
     );
