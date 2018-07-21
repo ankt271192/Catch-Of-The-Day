3523ae601b564e7df730b848f962eb40ec73be25
Ankit Tewari <ankt271192@users.noreply.github.com>
2018-07-21 16:29:52 +0530

    Add prop validation for components

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,12 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class EditFishForm extends React.Component {
+  static propTypes = {
+    fish: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    index: PropTypes.string,
+    updateFish: PropTypes.func
+  };
+
   handleChange = event => {
     // Update that fish
     // 1. Take a copy of the current fish
     const updatedFish = {
       ...this.props.fish,
-      [event.currentTarget.name]: event.currentTarget.value
+      [event.currentTarget.name]:
+        event.currentTarget.name === "price"
+          ? parseFloat(event.currentTarget.value)
+          : event.currentTarget.value
     };
     this.props.updateFish(this.props.index, updatedFish);
   };
